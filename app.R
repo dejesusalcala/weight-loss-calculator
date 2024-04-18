e0db156385001aa4cb78b0313d87a43ecfa38cce
Angel de Jesus Alcala Ruiz <dejesusalcala@gmail.com>
2024-04-18 01:51:56 -0700

    added widgets to web app

--- app.R
+++ app.R
@@ -13,16 +13,49 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Weight Loss Calculator"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+          selectInput(inputId = "SelectAge", 
+                      label = h4("Age"), 
+                      choices = c(16:80), 
+                      selected = 30),
+          radioButtons(inputId = "SelectGender", 
+                       label = h4("Gender"),
+                       choices = list("Male" = 1, 
+                                      "Female" = 2), 
+                       selected = 1),
+          selectInput(inputId = "SelectHeightFeet", 
+                      label = h4("Height (feet, inches)"), 
+                      choices = list("4 feet" = 4,
+                                     "5 feet" = 5,
+                                     "6 feet" = 6), 
+                      selected = 5),
+          selectInput(inputId = "SelectHeightInches", 
+                      label = "",
+                      choices = list("0 inches" = 0,
+                                     "1 inches" = 1,
+                                     "2 inches" = 2,
+                                     "3 inches" = 3,
+                                     "4 inches" = 4,
+                                     "5 inches" = 5,
+                                     "6 inches" = 6,
+                                     "7 inches" = 7,
+                                     "8 inches" = 8,
+                                     "9 inches" = 9,
+                                     "10 inches" = 10,
+                                     "11 inches" = 11), 
+                      selected = 9),
+          numericInput(inputId = "SelectWeight", 
+                       label = h4("Weight (lbs)"),
+                       value = 190),
             sliderInput("bins",
                         "Number of bins:",
                         min = 1,
                         max = 50,
-                        value = 30)
+                        value = 30),
         ),
 
         # Show a plot of the generated distribution
